14117f296a0d940f4509ee0d0ac602ec01838f17
elijah <2818195805@qq.com>
2023-06-03 21:39:49 +0800

    add load_model util in __init__

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
-from model import GazeGan
+import torch
+
+def load_model(model_path, device):
+    from model import GazeGan
+    model = GazeGan()
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.to(device)
+    model.eval()
+    return model
 
