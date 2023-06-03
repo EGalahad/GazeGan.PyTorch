54c64a89321d359192ca2485778c96cf9ba8d236
elijah <2818195805@qq.com>
2023-06-03 21:42:16 +0800

    fix relative path

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 import torch
 
+import os
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
 def load_model(model_path, device):
     from model import GazeGan
     model = GazeGan()
-    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.load_state_dict(torch.load(os.path.join(cur_dir, model_path), map_location=device))
     model.to(device)
     model.eval()
     return model
